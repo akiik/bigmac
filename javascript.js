5526d17900d35f885a0475b567288467dfe7033d
Nikolai Konovalov <biomech@ut.ee>
2013-03-11 12:06:13 +0200

    #13

--- javascript.js
+++ javascript.js
@@ -75,3 +75,35 @@ $(document).ready(function()
 		$("#myTable2").tablesorter(); 
     } 
 ); 
+
+ $(function() {
+$('#tabs li').click(function() {
+	loading();
+  	//alert("OOK");
+    });
+ });
+ 
+ 
+// Javascript
+
+function loading()
+{
+	document.getElementById("tabs").style.visibility="hidden";
+	var spinner = new Spinner({
+	lines: 12, // The number of lines to draw
+	length: 7, // The length of each line
+	width: 5, // The line thickness
+	radius: 10, // The radius of the inner circle
+	color: '#000', // #rbg or #rrggbb
+	speed: 1, // Rounds per second
+	trail: 100, // Afterglow percentage
+	shadow: true // Whether to render a shadow
+}).spin(document.getElementById("stat_table"));
+	
+	setTimeout(function() {
+        console.log("bouh");
+        document.getElementById("tabs").style.visibility="visible";
+        spinner.stop(); // Stop the spinner
+    }, 1000);
+
+}
